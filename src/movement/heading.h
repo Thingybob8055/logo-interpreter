41b40b5bd64969e07c3975af71d2084932b266a3
Akshay Gopinath <akshay8055.007@gmail.com>
2023-11-20 17:23:07 +0000

    #51: add heading enum file
    Driver: ag20751
    Navigator: mw20619
    
    
    git-svn-id: https://csee.essex.ac.uk/svn/ce320-06/trunk@77 48541511-ed4e-4a0e-8099-d0968ce73c23

---
+++ src/movement/heading.h
@@ -0,0 +1,13 @@
+#ifndef _HEADING_H_
+#define _HEADING_H_
+
+typedef enum Direction { FORWARDS, BACKWARDS } Direction;
+
+typedef enum Heading {
+  HEADING_UP,
+  HEADING_DOWN,
+  HEADING_LEFT,
+  HEADING_RIGHT
+} Heading;
+
+#endif /* _HEADING_H_ */
