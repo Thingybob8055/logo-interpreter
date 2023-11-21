83441f89c651b8f2b9c15032d143bfedf55f0bb3
Matthew Whitaker <matt@acmwhitaker.co.uk>
2023-11-21 17:33:14 +0000

    #48: Add more coverage for logo_parsing
    Driver: mw20619
    Navigator: fa20612
    
    
    git-svn-id: https://csee.essex.ac.uk/svn/ce320-06/trunk@85 48541511-ed4e-4a0e-8099-d0968ce73c23

--- test/logo_parser_test.cc
+++ test/logo_parser_test.cc
@@ -11,7 +11,8 @@ class LogoParserTestFixture : public ::testing::Test {
   virtual void TearDown() override { delete import; }
 };
 
-TEST_F(LogoParserTestFixture, correctly_read_commands_from_lgo_file) {
+TEST_F(LogoParserTestFixture,
+       Next_when_magnitude_read_command_with_magnitude_from_string) {
   import->SetContents("fd 100\nrt 90");
   Parser parser = Parser(import->GetContents());
   parser.Next();
@@ -23,10 +24,24 @@ TEST_F(LogoParserTestFixture, correctly_read_commands_from_lgo_file) {
 }
 
 TEST_F(LogoParserTestFixture,
-       correctly_read_command_without_magnitude_from_lgo_file) {
+       Next_when_no_magnitude_read_command_without_magnitude_from_string) {
   import->SetContents("pu");
   Parser parser = Parser(import->GetContents());
   parser.Next();
   ASSERT_EQ(parser.GetCommand(), KEY_SPACE);
   ASSERT_EQ(parser.GetMagnitude(), 0);
+}
+
+TEST_F(LogoParserTestFixture, HasNext_when_no_more_commands_then_false) {
+  import->SetContents("pu");
+  Parser parser = Parser(import->GetContents());
+  parser.Next();
+  ASSERT_FALSE(parser.HasNext());
+}
+
+TEST_F(LogoParserTestFixture, HasNext_when_more_commands_then_true) {
+  import->SetContents("pu\nfd 100");
+  Parser parser = Parser(import->GetContents());
+  parser.Next();
+  ASSERT_TRUE(parser.HasNext());
 }
