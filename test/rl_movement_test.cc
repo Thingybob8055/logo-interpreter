e1424ab59dd7f33a6609082e4723f737f1d81b1c
Femi Amusan <femiamusan@aol.co.uk>
2023-12-04 13:09:47 +0000

    #84: Create more tests for Raylib renderer. Improve code coverage TDD: Finsh tests for Raylib movement.
    Driver: fa20612
    Navigator: ag20751 
    
    
    git-svn-id: https://csee.essex.ac.uk/svn/ce320-06/trunk@112 48541511-ed4e-4a0e-8099-d0968ce73c23

--- test/rl_movement_test.cc
+++ test/rl_movement_test.cc
@@ -18,7 +18,7 @@ TEST_F(RLMovementTest, when_constructed_expect_rl_movement_to_be_created) {
   int y_safe_zone = 50;
   RLCharacterAssembler assembler;
 
-  RLMovement movement(x_location, y_location, x_safe_zone, y_safe_zone,
+  RLMovement movement(y_location, x_location, y_safe_zone, x_safe_zone,
                       &assembler);
   EXPECT_EQ(movement.GetXLocation(), x_location);
   EXPECT_EQ(movement.GetYLocation(), y_location);
@@ -35,7 +35,7 @@ TEST_F(RLMovementTest,
   int y_safe_zone = 50;
   RLCharacterAssembler assembler;
 
-  RLMovement movement(x_location, y_location, x_safe_zone, y_safe_zone,
+  RLMovement movement(y_location, x_location, y_safe_zone, x_safe_zone,
                       &assembler);
   movement.MoveForward();
   EXPECT_EQ(cosfWrapper_fake.call_count, 1);
@@ -50,7 +50,7 @@ TEST_F(RLMovementTest,
   int y_safe_zone = 50;
   RLCharacterAssembler assembler;
 
-  RLMovement movement(x_location, y_location, x_safe_zone, y_safe_zone,
+  RLMovement movement(y_location, x_location, y_safe_zone, x_safe_zone,
                       &assembler);
   movement.MoveBackward();
   EXPECT_EQ(cosfWrapper_fake.call_count, 1);
@@ -64,10 +64,25 @@ TEST_F(RLMovementTest, when_turn_right_90_expect_angle_to_be_updated) {
   int y_safe_zone = 50;
   RLCharacterAssembler assembler;
 
-  RLMovement movement(x_location, y_location, x_safe_zone, y_safe_zone,
+  RLMovement movement(y_location, x_location, y_safe_zone, x_safe_zone,
                       &assembler);
   movement.TurnRight90();
   EXPECT_EQ((int)movement.GetCurrentHeadingFromAssembler(), 360);
+}
+
+TEST_F(RLMovementTest,
+       when_turn_right_called_and_angle_is_less_than_0_expect_angle_minus_360) {
+  int x_location = 50;
+  int y_location = 50;
+  int x_safe_zone = 50;
+  int y_safe_zone = 50;
+  RLCharacterAssembler assembler;
+
+  RLMovement movement(y_location, x_location, y_safe_zone, x_safe_zone,
+                      &assembler);
+  assembler.SetCurrentHeading((Heading)-180);
+  movement.TurnRight90();
+  EXPECT_EQ((int)movement.GetCurrentHeadingFromAssembler(), 450);
 }
 
 TEST_F(RLMovementTest, when_turn_left_90_expect_angle_to_be_updated) {
@@ -77,8 +92,110 @@ TEST_F(RLMovementTest, when_turn_left_90_expect_angle_to_be_updated) {
   int y_safe_zone = 50;
   RLCharacterAssembler assembler;
 
-  RLMovement movement(x_location, y_location, x_safe_zone, y_safe_zone,
+  RLMovement movement(y_location, x_location, y_safe_zone, x_safe_zone,
                       &assembler);
   movement.TurnLeft90();
   EXPECT_EQ((int)movement.GetCurrentHeadingFromAssembler(), 180);
+}
+
+TEST_F(
+    RLMovementTest,
+    when_turn_left_called_and_angle_is_greater_than_360_expect_angle_plus_360) {
+  int x_location = 50;
+  int y_location = 50;
+  int x_safe_zone = 50;
+  int y_safe_zone = 50;
+  RLCharacterAssembler assembler;
+
+  RLMovement movement(y_location, x_location, y_safe_zone, x_safe_zone,
+                      &assembler);
+  assembler.SetCurrentHeading((Heading)720);
+  movement.TurnLeft90();
+  EXPECT_EQ((int)movement.GetCurrentHeadingFromAssembler(), 990);
+}
+
+TEST_F(
+    RLMovementTest,
+    when_turn_right_called_and_current_angle_less_than_0_expect_360_minus_current_angle) {
+  int x_location = 50;
+  int y_location = 50;
+  int x_safe_zone = 50;
+  int y_safe_zone = 50;
+  RLCharacterAssembler assembler;
+
+  RLMovement movement(y_location, x_location, y_safe_zone, x_safe_zone,
+                      &assembler);
+  assembler.SetCurrentHeading((Heading)-180);
+  movement.TurnRight(90);
+  EXPECT_EQ((int)movement.GetCurrentHeadingFromAssembler(), 450);
+}
+
+TEST_F(
+    RLMovementTest,
+    when_turn_left_called_and_current_angle_greater_than_360_expect_current_angle_plus_360) {
+  int x_location = 50;
+  int y_location = 50;
+  int x_safe_zone = 50;
+  int y_safe_zone = 50;
+  RLCharacterAssembler assembler;
+
+  RLMovement movement(y_location, x_location, y_safe_zone, x_safe_zone,
+                      &assembler);
+  assembler.SetCurrentHeading((Heading)720);
+  movement.TurnLeft(90);
+  EXPECT_EQ((int)movement.GetCurrentHeadingFromAssembler(), 990);
+}
+
+TEST_F(RLMovementTest, when_at_bottom_of_the_screen_wrap_y_location) {
+  int x_location = 51;
+  int y_location = 51;
+  int x_safe_zone = 50;
+  int y_safe_zone = 50;
+  RLCharacterAssembler assembler;
+
+  RLMovement movement(y_location, x_location, y_safe_zone, x_safe_zone,
+                      &assembler);
+  movement.MoveBackward();
+  EXPECT_EQ(movement.GetYLocation(), 0);
+}
+
+TEST_F(RLMovementTest, when_at_top_of_the_screen_wrap_y_location) {
+  int x_location = 25;
+  int y_location = -1;
+  int x_safe_zone = 50;
+  int y_safe_zone = 50;
+  RLCharacterAssembler assembler;
+
+  RLMovement movement(y_location, x_location, y_safe_zone, x_safe_zone,
+                      &assembler);
+  movement.MoveForward();
+  EXPECT_EQ(movement.GetYLocation(), y_safe_zone);
+}
+
+TEST_F(RLMovementTest, when_at_left_of_the_screen_wrap_x_location) {
+  int x_location = -1;
+  int y_location = 25;
+  int x_safe_zone = 50;
+  int y_safe_zone = 50;
+  RLCharacterAssembler assembler;
+
+  RLMovement movement(y_location, x_location, y_safe_zone, x_safe_zone,
+                      &assembler);
+  movement.TurnLeft90();
+  movement.MoveForward();
+  EXPECT_EQ(movement.GetXLocation(), x_safe_zone);
+}
+
+TEST_F(RLMovementTest, when_at_right_of_the_screen_wrap_x_location) {
+  int x_location = 51;
+  int y_location = 25;
+  int x_safe_zone = 50;
+  int y_safe_zone = 50;
+  RLCharacterAssembler assembler;
+
+  RLMovement movement(y_location, x_location, y_safe_zone, x_safe_zone,
+                      &assembler);
+  movement.TurnRight90();
+  movement.MoveForward();
+  EXPECT_EQ(movement.GetXLocation(), 0);
 }
