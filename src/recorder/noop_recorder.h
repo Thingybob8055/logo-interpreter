906025daba4c4491276ac64c7079c9d5fb02303b
Matthew Whitaker <matt@acmwhitaker.co.uk>
2023-12-04 16:27:59 +0000

    #85: Create NoopRecorder for PD
    git-svn-id: https://csee.essex.ac.uk/svn/ce320-06/trunk@114 48541511-ed4e-4a0e-8099-d0968ce73c23

---
+++ src/recorder/noop_recorder.h
@@ -0,0 +1,8 @@
+#include "recorder/recorder.h"
+
+class NoopRecorder : public Recorder {
+ public:
+  void InitRecording() override { return; }
+  void StartRecording() override { return; }
+  void SaveRecording() override { return; }
+};
