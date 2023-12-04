d364972d66c12e9510695db4b35ae82011daca71
Akshay Gopinath <akshay8055.007@gmail.com>
2023-12-04 16:19:33 +0000

    #85: Add recorder interface
    Driver: ag20751
    Navigator: mw20619, sg21642
    
    
    git-svn-id: https://csee.essex.ac.uk/svn/ce320-06/trunk@113 48541511-ed4e-4a0e-8099-d0968ce73c23

---
+++ src/recorder/recorder.h
@@ -0,0 +1,12 @@
+#ifndef RECORDING_H_
+#define RECORDING_H_
+
+class Recorder {
+ public:
+  virtual void InitRecording() = 0;
+  virtual void StartRecording() = 0;
+  virtual void SaveRecording() = 0;
+  virtual ~Recorder() = default;
+};
+
+#endif /* RECORDING_H_ */
