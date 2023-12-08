81d46f29672e585e7ea805894fe4543d3c6fa8ee
Matthew Whitaker <matt@acmwhitaker.co.uk>
2023-12-08 14:15:33 +0000

    #70: Increase test coverage on nested loops
    Driver: mw20619
    Navigator: fa20612 
    
    
    git-svn-id: https://csee.essex.ac.uk/svn/ce320-06/trunk@133 48541511-ed4e-4a0e-8099-d0968ce73c23

--- test/logo_line_break_handler_test.cc
+++ test/logo_line_break_handler_test.cc
@@ -26,6 +26,17 @@ TEST(
 
 TEST(
     LogoLineBreakHandlerTest,
+    when_given_repeat_should_not_break_line) {
+  std::stringstream ss;
+  ss << "pu repeat 4 [ fd 50 rt 90 ] pd";
+  auto handler = LogoLineBreakHandler();
+  auto out = handler.Handle(ss);
+  std::string expected = "pu\nrepeat 4 [ fd 50 rt 90 ]\npd\n";
+  EXPECT_EQ(out.str(), expected);
+}
+
+TEST(
+    LogoLineBreakHandlerTest,
     when_given_a_two_commands_on_separate_lines_then_return_the_command_with_a_line_break) {
   std::stringstream ss;
   ss << "fd 50\nrt 90";
